9b1b17c6dfa0d8b7ae2cf155ce628efdf193ec03
2017010083索高辉 <48475930+Suogh@users.noreply.github.com>
2019-09-18 23:14:18 +0800

    Add files via upload

---
+++ WindowsForms/WindowsForms/MyForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsForms
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOpenFile = new System.Windows.Forms.Button();
+            this.btSaveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btOpenFile
+            // 
+            this.btOpenFile.Location = new System.Drawing.Point(120, 435);
+            this.btOpenFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btOpenFile.Name = "btOpenFile";
+            this.btOpenFile.Size = new System.Drawing.Size(231, 80);
+            this.btOpenFile.TabIndex = 0;
+            this.btOpenFile.Text = "OpenFile";
+            this.btOpenFile.UseVisualStyleBackColor = true;
+            this.btOpenFile.Click += new System.EventHandler(this.btOpenFile_Click);
+            // 
+            // btSaveFile
+            // 
+            this.btSaveFile.Location = new System.Drawing.Point(679, 435);
+            this.btSaveFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btSaveFile.Name = "btSaveFile";
+            this.btSaveFile.Size = new System.Drawing.Size(233, 80);
+            this.btSaveFile.TabIndex = 1;
+            this.btSaveFile.Text = "SaveFile";
+            this.btSaveFile.UseVisualStyleBackColor = true;
+            this.btSaveFile.Click += new System.EventHandler(this.btSaveFile_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 568);
+            this.Controls.Add(this.btSaveFile);
+            this.Controls.Add(this.btOpenFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MyForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MyForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSaveFile;
+        private System.Windows.Forms.Button btOpenFile;
+    }
+}
+
---
+++ WindowsForms/WindowsForms/MyForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WindowsForms
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog opDlg = new OpenFileDialog();
+            opDlg.Multiselect = true;
+            opDlg.ShowDialog();
+            m_dir = opDlg.FileNames;
+            length = m_dir.Length;
+
+        }
+
+        private void btSaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svDlg = new SaveFileDialog();
+            svDlg.ShowDialog();
+            string dir;
+            for (int i = 0; i < length; i++)
+            {
+                index = m_dir[i].LastIndexOf("\\");
+                m_name = m_dir[i].Substring(index + 1);
+                string get = svDlg.FileName;
+                index = get.LastIndexOf("\\");
+                dir = get.Substring(0, get.LastIndexOf("\\") + 1);
+                dir = dir + m_name;
+                System.IO.File.Copy(m_dir[i], dir);
+            }
+        }
+        string[] m_dir;
+        int length;
+        string m_name;
+        int index;
+
+        private void MyForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
