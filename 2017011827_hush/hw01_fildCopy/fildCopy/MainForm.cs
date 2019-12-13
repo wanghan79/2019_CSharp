ebda78dc51a8715c9a58b688f076c8631e8e72b0
allenhush <1324816813@qq.com>
2019-12-13 18:35:18 +0800

    胡顺航：第一次作业

---
+++ 2017011827_hush/hw01_fildCopy/fildCopy/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fildCopy
+{
+    public partial class MainForm : Form
+    {
+        string pickPath = "";
+        string pickName = "";
+        string savePath = "";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_pickFild_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pickDialog = new OpenFileDialog();
+            pickDialog.Multiselect = true;//该值确定是否可以选择多个文件
+            pickDialog.Title = "请选择文件夹";
+            pickDialog.Filter = "所有文件(*.*)|*.*";
+            if (pickDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pickPath = pickDialog.FileName;
+                pickName = System.IO.Path.GetFileName(pickDialog.FileName);
+                lab_pickFildName.Text = pickPath;
+            }
+        }
+
+        private void btn_copyTo_click(object sender, EventArgs e)
+        {
+            if (pickPath == "")
+            {
+                lab_pickFildName.Text = "请先选择需要复制的文件";
+            }
+            else
+            {
+                System.Windows.Forms.FolderBrowserDialog saveDialog = new System.Windows.Forms.FolderBrowserDialog();
+                if (saveDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    savePath = saveDialog.SelectedPath;
+                    lab_savePath.Text = savePath;
+                }
+            }
+        }
+
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            lab_pickFildName.Text = "";
+            lab_savePath.Text = "";
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            File.Copy(@pickPath, @savePath + @"\" + @pickName, true);
+            MessageBox.Show("复制成功");
+        }
+
+        private void lab_pickFildName_Click(object sender, EventArgs e)
+        {
+            this.btn_pickFild_Click( sender,  e);
+        }
+
+        private void lab_savePath_Click(object sender, EventArgs e)
+        {
+            this.btn_copyTo_click(sender, e);
+        }
+    }
+}
