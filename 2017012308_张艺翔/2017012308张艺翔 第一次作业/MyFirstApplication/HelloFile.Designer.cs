95d4f51368a2629cd290b1c0dae2fb23eb8e8b33
2017012308张艺翔 <45139180+ElroyLR@users.noreply.github.com>
2019-12-19 09:39:16 +0800

    zhangyx

---
+++ 2017012308_张艺翔/2017012308张艺翔 第一次作业/MyFirstApplication/HelloFile.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MyFirstApplication
+{
+    partial class HelloFile
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.SaveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(72, 82);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(207, 91);
+            this.OpenFile.TabIndex = 0;
+            this.OpenFile.Text = "OpenFile";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Location = new System.Drawing.Point(417, 87);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(210, 81);
+            this.SaveFile.TabIndex = 1;
+            this.SaveFile.Text = "SaveFile";
+            this.SaveFile.UseVisualStyleBackColor = true;
+            this.SaveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // HelloFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 387);
+            this.Controls.Add(this.SaveFile);
+            this.Controls.Add(this.OpenFile);
+            this.Name = "HelloFile";
+            this.Text = "FileOperation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.Button SaveFile;
+    }
+}
+
