0dc45274e7186d6e337d0a0e558ca1231cb3cb17
2017010062王玉虎 <49155366+wangyhu@users.noreply.github.com>
2019-12-23 21:37:57 +0800

    王玉虎第一次作业

---
+++ MyForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "请选择文件!";
+            openFileDialog1.Filter = "所有文件(*.*)|*.";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = "D:\\";
+            saveFileDialog1.Filter = "(*.*)|*.";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+        private void FileFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog1 = new FontDialog();
+            fontDialog1.ShowColor = true;
+            fontDialog1.Font = richTextBox1.SelectionFont;
+            fontDialog1.Color = richTextBox1.SelectionColor;
+            if(fontDialog1.ShowDialog()!=DialogResult.Cancel)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+                richTextBox1.SelectionColor = fontDialog1.Color;
+            }
+        }
+
+        private void FileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
